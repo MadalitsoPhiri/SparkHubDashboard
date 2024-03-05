8d39fcfad54e71a1ef1406ebed3fd340250fd7a7
Kitanga Nday <kitanganday@gmail.com>
2024-03-05 16:23:10 +0200

    feat: added new colour calculations util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -175,3 +175,15 @@ export const formatActivityData = (activityLog: ActivityLog[]) => {
 
   return result;
 };
+
+export const pickTextColorBasedOnBgColorAdvanced = (
+  bgColor: string,
+  lightColor: string,
+  darkColor: string,
+) => {
+  const color = bgColor.charAt(0) === '#' ? bgColor.substring(1, 7) : bgColor;
+  const r = parseInt(color.substring(0, 2), 16); // hexToR
+  const g = parseInt(color.substring(2, 4), 16); // hexToG
+  const b = parseInt(color.substring(4, 6), 16); // hexToB
+  return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkColor : lightColor;
+};
